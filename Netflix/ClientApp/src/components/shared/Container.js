fb71d0e31cbef3a539046b0317060a0699112fa5
Lulubul <robert.unguru@gmail.com>
2019-02-09 13:22:11 +0200

    Update to react 16.4 and refactor navigation arrows

--- Netflix/ClientApp/src/components/shared/Container.js
+++ Netflix/ClientApp/src/components/shared/Container.js
@@ -2,48 +2,97 @@ import React, { Component } from 'react'
 import { Item } from './Item';
 import './Container.css';
 
-export const Direction = { Back : 1, Foward : -1 }
+export const Direction = { Back : -1, Forward: 1 }
+
+const NextButton = ({clickEvent}) => <a className="carousel-control right" role="button" onClick={() => clickEvent()}>
+        <span className="glyphicon glyphicon-chevron-right"></span>
+    </a>;
+
+const PreviousButton = ({clickEvent}) => <a className="carousel-control left" role="button" onClick={() => clickEvent()}>
+        <span className="glyphicon glyphicon-chevron-left"></span>
+    </a>;
 
 export class Container extends Component {
 
-    stepSize = 1750;
-
     constructor(props) {
         super(props);
-        this.state = { position: 0 };
+
+        this.itemsRef = React.createRef();
+        this.state = { position: 0, index: 0, stepSize: 1750, elementWidth: 0, containerSize: 1 };
     }
-    
+
+    componentDidMount() {
+        window.addEventListener('resize', () => this.updateWindowDimensions(this.props.items.length));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions(this.props.items.length));
+    }
+
+    componentWillReceiveProps = (props) => {
+        this.updateWindowDimensions(props.items.length);
+    }
+
+    updateWindowDimensions = (elementCount) => {
+        const elementWidth = document.getElementsByClassName('boxart-container')[0].offsetWidth;
+        const stepSize = window.innerWidth - 120;
+        const elementsInContainer = stepSize / elementWidth;
+        const containerSize = Math.ceil(elementCount / elementsInContainer);
+        this.setState({ ... stepSize, elementWidth, containerSize  });
+    }
+
     goBack = (event) => {
         this.goToNextPosition(Direction.Back);
     }
 
     goNext = (event) => {
-        this.goToNextPosition(Direction.Foward);
+        this.goToNextPosition(Direction.Forward);
+    }
+
+    goToElement = (elementId) => {
+
+    }
+
+    hasNextButton = () => {
+        return this.state.index < this.state.containerSize - 1;
+    }
+
+    hasPreviousButton = () => {
+        return this.state.index > 0;
     }
     
     goToNextPosition(direction) {
-        const nextPosition = this.state.position + direction * this.stepSize;
-        this.setState({ position: nextPosition });
-        console.log(this.state.position);
-        document.getElementsByClassName("items")[0].style.transform = "translateX(" + nextPosition + "px)";
+        const {index, position, stepSize} = this.state;
+        const nextPosition = position - direction * stepSize;
+        const nextIndex = index + direction;
+        this.setState({ position: nextPosition, index: nextIndex });
+
+        this.itemsRef.current.style.transform = "translateX(" + nextPosition + "px)";
     }
 
     render() {
         const { title, items, size } = this.props;
         return (
             <div>
-                <h2>{title}</h2>
+                <div>
+                    <h2>{title}</h2>
+                    <ol className="carousel-indicators">
+                        
+                    </ol>
+                </div>
                 <div className="rowContainer">
-                    <div className="handle handlePrev active" role="button" aria-label="See more titles" onClick={this.goBack}>
-                        <b className="carousel-control-prev-icon"></b>
+                    <div className="handle handlePrev active">
+                        {this.hasPreviousButton() ? <PreviousButton clickEvent={() => this.goBack()} /> : ""}
                     </div>
                     <div className="sliderMask">
-                        <div className="items">
-                            {items.map((item, index) => (<Item key={index} imageSource={item.image} size={size}></Item>))}
+                        <div className="items" ref={this.itemsRef}>
+                            {items.map((item, index) => (
+                                <Item key={index} imageSource={item.image} size={size} onClick={this.goToElement(item.videoId)} />
+                            ))}
                         </div>
                     </div>
-                    <div className="handle handleNext active" role="button" aria-label="See more titles" onClick={this.goNext}>
-                        <b className="carousel-control-next-icon"></b>
+                    <div className="handle handleNext active">
+                        {this.hasNextButton() ? <NextButton clickEvent={() => this.goNext()} /> : ""}
                     </div>
                 </div>
             </div>
