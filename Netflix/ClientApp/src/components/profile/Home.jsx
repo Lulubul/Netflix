ad6268b93af187b880ec74e839d4e14ac9ca85ae
Lulubul <robert.unguru@gmail.com>
2019-03-05 19:12:26 +0200

    Fix style after upgrade to bootstrap 4

--- Netflix/ClientApp/src/components/profile/Home.jsx
+++ Netflix/ClientApp/src/components/profile/Home.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { Col, Container, Row, Button} from 'react-bootstrap';
-import { Profile } from './Profile';
-import { getProfiles } from '../../resources/Api';
-import { Link } from 'react-router-dom';
-import './Home.css';
+import React, { Component } from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
+import { Profile } from "./Profile";
+import { getProfiles } from "../../resources/Api";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export class Home extends Component {
-  displayName = Home.name
+  displayName = Home.name;
   constructor(props) {
     super(props);
     this.state = { profiles: [] };
   }
 
   componentDidMount() {
-    const userId = '3f008259-8509-40a2-8118-f047861e4f31';
-    getProfiles(userId)
-      .then((profiles) => this.setState({profiles: profiles}))
+    const userId = "3f008259-8509-40a2-8118-f047861e4f31";
+    getProfiles(userId).then(profiles => this.setState({ profiles: profiles }));
   }
 
   render() {
@@ -27,21 +27,23 @@ export class Home extends Component {
         <Container lg={12} md={12}>
           <Row>
             {profiles.map((profile, index) => (
-              <Col key={index} className="profile-wrapper" xs={2} md={2} lg={2}> 
-                <Profile key={index} profile={profile}/> 
+              <Col key={index} className="profile-wrapper" xs={2} md={2} lg={2}>
+                <Profile key={index} profile={profile} />
               </Col>
             ))}
             <Col className="profile-wrapper" xs={2} md={2} lg={2}>
-                <div>
-                  <Link to={'/newProfile'}>
-                    <span className="glyphicon glyphicon-plus-sign"></span>
-                    <span className="profile-name">Add Profile</span>
-                  </Link>
-                </div>
+              <div>
+                <Link to={"/newProfile"}>
+                  <FontAwesomeIcon icon="plus-circle" />
+                  <span className="profile-name">Add Profile</span>
+                </Link>
+              </div>
             </Col>
           </Row>
           <Row>
-            <Button id="manage-profile" size="lg">Manage profiles</Button>
+            <Button id="manage-profile" size="lg">
+              Manage profiles
+            </Button>
           </Row>
         </Container>
       </div>
